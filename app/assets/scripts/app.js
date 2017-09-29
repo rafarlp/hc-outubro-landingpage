c9184eac1cbca4868fd09812aee5295a7c74049b
Rafael L. Pereira <rafaelleop@gmail.com>
2017-09-29 07:51:17 -0300

    Responsive layout adjustments

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,16 @@
-var $ = require('jquery');
+import ScrollReveal from 'scrollreveal';
+import SmoothScroll from './modules/smoothscroll';
 
-alert('ABC 123');
+var smoothScroll = new SmoothScroll();
+
+window.sr = ScrollReveal({ reset: false, distance: '24px', duration: 1500, scale: 1 });
+sr.reveal('.promo-intro h1', { scale: 1.2, origin: 'top' });
+sr.reveal('.promo-intro h2', { scale: 1.2, delay: 500});
+sr.reveal('.promo-intro h3', { delay: 1000});
+sr.reveal('.promo-intro .btn', { scale: 1, delay: 1000});
+sr.reveal('.hero-shot', { rotate: { y: 40 }, distance: '200px', delay: 1000});
+sr.reveal('nav, .logo', { origin: 'top', delay: 1000});
+sr.reveal('.phone', { origin: 'bottom right', distance: '60px', rotate: { z: 10 }, delay: 200});
+sr.reveal('.laptop, h1, h2, h3, .btn, .client-carousel, .contact-form', { duration: 1000 });
+sr.reveal('.feature-item', { duration: 2000 }, 100);
+sr.reveal('.info-text p', 150);
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./app/temp/scripts"),
     filename: "app.js"
-  }
+  },
   module: {
     loaders: [
       {
